c8789c6582fe0498eb5a7f46f529d753f9baaf31
awesomejosh0308 <162935166+awesomejosh0308@users.noreply.github.com>
2024-07-20 17:14:41 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+console.log("My first console project");
+
+var first_name = "Joshua ";
+var last_name = "Yan";
+var comb_name = first_name.concat(last_name);
+console.log(comb_name);
+
+var number1 = "10";
+var number2 = "5";
+var add = Number(number1) + Number(number2);
+console.log(add);
+var sub = number1 - number2;
+console.log(sub);
+var multi = number1 * number2;
+console.log(multi);
+var divide = number1 / number2;
+console.log(divide);
